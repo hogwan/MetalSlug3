fa2dc1af315db3b5622962b4ea02ded7354e10a9
asdf36597 김관호 <asdf36597@naver.com>
2024-01-23 14:55:36 +0900

    api 기본 구조

---
+++ MetalSlug3/MetalSlug3/MetalSlug3Core.cpp
@@ -0,0 +1,21 @@
+#include "MetalSlug3Core.h"
+
+MetalSlug3Core::MetalSlug3Core()
+{
+}
+
+MetalSlug3Core::~MetalSlug3Core()
+{
+}
+
+void MetalSlug3Core::EngineStart()
+{
+}
+
+void MetalSlug3Core::EngineUpdate()
+{
+}
+
+void MetalSlug3Core::EngineEnd()
+{
+}
---
+++ MetalSlug3/MetalSlug3/MetalSlug3Core.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <EngineCore/EngineCore.h>
+class MetalSlug3Core : public EngineCore
+{
+public:
+	// constructor destructor
+	MetalSlug3Core();
+	~MetalSlug3Core();
+
+	// delete Function
+	MetalSlug3Core(const MetalSlug3Core& _Other) = delete;
+	MetalSlug3Core(MetalSlug3Core&& _Other) noexcept = delete;
+	MetalSlug3Core& operator=(const MetalSlug3Core& _Other) = delete;
+	MetalSlug3Core& operator=(MetalSlug3Core&& _Other) = delete;
+protected:
+	void EngineStart() override;
+	void EngineUpdate() override;
+	void EngineEnd() override;
+
+private:
+};
+
