46f63243b57a32a2b2ee45154394870270f6cb9d
asdf36597 김관호 <asdf36597@naver.com>
2024-01-29 16:16:29 +0900

    리소스매니저, 경로설정

--- MetalSlug3/MetalSlug3/PlayLevel.cpp
+++ MetalSlug3/MetalSlug3/PlayLevel.cpp
@@ -1,5 +1,8 @@
 #include "PlayLevel.h"
 #include "Marco.h"
+#include <EngineCore\EngineResourcesManager.h>
+#include <EngineBase\EngineDirectory.h>
+#include <EngineBase\EngineFile.h>
 
 PlayLevel::PlayLevel()
 {
@@ -11,5 +14,18 @@ PlayLevel::~PlayLevel()
 
 void PlayLevel::BeginPlay()
 {
+	UEngineDirectory NewPath;
+	NewPath;
+	NewPath.MoveParent();
+	NewPath;
+	NewPath.MoveParent();
+	NewPath;
+	NewPath.Move("Resource");
+	NewPath;
+	NewPath.Move("Marco");
+	NewPath;
+
+	std::list<UEngineFile> AllFileList = NewPath.AllFile({ ".png", ".bmp" }, true);
+	
 	SpawnActor<Marco>();
 }
