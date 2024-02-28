126b162807fb7b13671fe7946cdd66c2fd387878
asdf36597 김관호 <asdf36597@naver.com>
2024-02-28 11:07:52 +0900

    ㄴㄴㄴㄴ

--- MetalSlug3/Copy.bat
+++ MetalSlug3/Copy.bat
@@ -1,8 +1,9 @@
 @echo off 
-for /f "tokens=* delims=" %%a in ('dir /b/s C:\GM\WIn\EngineBase\*.h') do copy /y "%%a" "C:\MetalSlug3\MetalSlug3\EngineBase\"
-for /f "tokens=* delims=" %%a in ('dir /b/s C:\GM\WIn\EngineCore\*.h') do copy /y "%%a" "C:\MetalSlug3\MetalSlug3\EngineCore\"
-for /f "tokens=* delims=" %%a in ('dir /b/s C:\GM\WIn\EnginePlatform\*.h') do copy /y "%%a" "C:\MetalSlug3\MetalSlug3\EnginePlatform\"
+xcopy C:\GM\WIn\EngineBase\*.h C:\MetalSlug3\MetalSlug3\EngineBase\ /e /h /k /y
+xcopy C:\GM\WIn\EngineCore\*.h C:\MetalSlug3\MetalSlug3\EngineCore\ /e /h /k /y
+xcopy C:\GM\WIn\EnginePlatform\*.h C:\MetalSlug3\MetalSlug3\EnginePlatform\ /e /h /k /y
 
-for /f "tokens=* delims=" %%a in ('dir /b/s C:\GM\WIn\EngineBase\*.cpp') do copy /y "%%a" "C:\MetalSlug3\MetalSlug3\EngineBase\"
-for /f "tokens=* delims=" %%a in ('dir /b/s C:\GM\WIn\EngineCore\*.cpp') do copy /y "%%a" "C:\MetalSlug3\MetalSlug3\EngineCore\"
-for /f "tokens=* delims=" %%a in ('dir /b/s C:\GM\WIn\EnginePlatform\*.cpp') do copy /y "%%a" "C:\MetalSlug3\MetalSlug3\EnginePlatform\"
+@echo off 
+xcopy C:\GM\WIn\EngineBase\*.cpp C:\MetalSlug3\MetalSlug3\EngineBase\ /e /h /k /y
+xcopy C:\GM\WIn\EngineCore\*.cpp C:\MetalSlug3\MetalSlug3\EngineCore\ /e /h /k /y
+xcopy C:\GM\WIn\EnginePlatform\*.cpp C:\MetalSlug3\MetalSlug3\EnginePlatform\ /e /h /k /y
